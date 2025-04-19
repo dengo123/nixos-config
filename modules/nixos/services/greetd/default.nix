b39d4564c6d13469c5b0d2aa088b75d0a2247cbf
dengo123 <deniz060198@hotmail.com>
2025-04-19 16:21:46 +0200

    commit minor greetd changes

--- modules/nixos/services/greetd/default.nix
+++ modules/nixos/services/greetd/default.nix
@@ -1,5 +1,4 @@
 {
-  options,
   config,
   lib,
   pkgs,
@@ -7,26 +6,27 @@
   ...
 }:
 with lib;
-with lib.${namespace};
-let
+with lib.${namespace}; let
   cfg = config.${namespace}.services.greetd;
-  name = "ryan";
-in
-{
-  options.${namespace}.services.greetd = with types; {
-    enable = mkBoolOpt false "Enable greetd";
+in {
+  options.${namespace}.services.greetd = {
+    enable = mkBoolOpt false "Enable greetd display manager";
+    theme = mkOpt types.str "tuigreet" "Greeter theme to use (e.g. tuigreet, gtkgreet, ags, custom)";
   };
 
   config = mkIf cfg.enable {
     services.greetd = {
       enable = true;
       settings.default_session.command = pkgs.writeShellScript "greeter" ''
-        export XKB_DEFAULT_LAYOUT=${config.services.xserver.xkb.layout}
-        export XCURSOR_THEME=Qogir
-        ${name}/bin/greeter
+        export XKB_DEFAULT_LAYOUT=${config.services.xserver.xkb.layout or "us"}
+        export XCURSOR_THEME="Qogir"
+
+        ${cfg.theme}
       '';
     };
 
-    systemd.tmpfiles.rules = [ "d '/var/cache/greeter' - greeter greeter - -" ];
+    systemd.tmpfiles.rules = [
+      "d /var/cache/greeter 0755 greeter greeter -"
+    ];
   };
 }
