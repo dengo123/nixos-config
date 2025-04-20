cc34449495b2245ce4b0cbc19e67eb6b28e682cb
dengo123 <deniz060198@hotmail.com>
2025-04-20 18:21:50 +0200

    commit extendet lib/module

--- lib/module/default.nix
+++ lib/module/default.nix
@@ -1,74 +1,50 @@
 {lib, ...}:
 with lib; rec {
-  ## Create a NixOS module option.
-  ##
-  ## ```nix
-  ## lib.mkOpt nixpkgs.lib.types.str "My default" "Description of my option."
-  ## ```
-  ##
-  #@ Type -> Any -> String
+  ## === UNIVERSAL OPTION HELPERS ===
+
+  ## Create a general module option.
+  ## Example: mkOpt types.str "default" "description"
   mkOpt = type: default: description:
     mkOption {inherit type default description;};
 
-  ## Create a NixOS module option without a description.
-  ##
-  ## ```nix
-  ## lib.mkOpt' nixpkgs.lib.types.str "My default"
-  ## ```
-  ##
-  #@ Type -> Any -> String
-  mkOpt' = type: default: mkOpt type default null;
+  ## Same as mkOpt, but without description
+  mkOpt' = type: default:
+    mkOpt type default null;
 
-  ## Create a boolean NixOS module option.
-  ##
-  ## ```nix
-  ## lib.mkBoolOpt true "Description of my option."
-  ## ```
-  ##
-  #@ Type -> Any -> String
+  ## === BOOL ===
   mkBoolOpt = mkOpt types.bool;
-
-  ## Create a boolean NixOS module option without a description.
-  ##
-  ## ```nix
-  ## lib.mkBoolOpt true
-  ## ```
-  ##
-  #@ Type -> Any -> String
   mkBoolOpt' = mkOpt' types.bool;
 
+  ## === STRING ===
+  mkStrOpt = mkOpt types.str;
+  mkStrOpt' = mkOpt' types.str;
+
+  ## === INT ===
+  mkIntOpt = mkOpt types.int;
+  mkIntOpt' = mkOpt' types.int;
+
+  ## === ENUM ===
+  mkEnumOpt = values: default: description:
+    mkOpt (types.enum values) default description;
+
+  mkEnumOpt' = values: default:
+    mkEnumOpt values default null;
+
+  ## === LIST OF STRINGS ===
+  mkStrListOpt = mkOpt (types.listOf types.str);
+  mkStrListOpt' = mkOpt' (types.listOf types.str);
+
+  ## === LIST OF PACKAGES ===
+  mkPkgListOpt = mkOpt (types.listOf types.package);
+  mkPkgListOpt' = mkOpt' (types.listOf types.package);
+
+  ## === PREDEFINED SHORTCUTS ===
+
   enabled = {
-    ## Quickly enable an option.
-    ##
-    ## ```nix
-    ## services.nginx = enabled;
-    ## ```
-    ##
-    #@ true
     enable = true;
   };
 
   disabled = {
-    ## Quickly disable an option.
-    ##
-    ## ```nix
-    ## services.nginx = enabled;
-    ## ```
-    ##
-    #@ false
     enable = false;
   };
-  ## Create a string NixOS module option.
-  ##
-  ## ```nix
-  ## lib.mkStrOpt "default" "Description of the string option."
-  ## ```
-  mkStrOpt = mkOpt types.str;
-
-  ## Optional: ohne Beschreibung
-  mkStrOpt' = mkOpt' types.str;
-
-  ## Weitere Typen (wenn du willst)
-  # mkIntOpt = mkOpt types.int;
-  # mkStrListOpt = mkOpt (types.listOf types.str);
 }
