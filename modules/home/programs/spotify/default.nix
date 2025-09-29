9fbcaeac28816cbf1898d7efd7a460d0a68443e4
Dein Name <deniz060198@hotmail.com>
2025-09-29 11:34:28 +0200

    personal spotify

--- modules/home/programs/spotify/default.nix
+++ modules/home/programs/spotify/default.nix
@@ -15,6 +15,10 @@ in {
   config = mkIf cfg.enable {
     programs.spotify-player = {
       enable = true;
+      settings = {
+        client_id = "042e17bf925744edb90d80b4b045ceb7";
+        login_redirect_uri = "http://127.0.0.1:8989/login";
+      };
     };
   };
 }
