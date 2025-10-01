94f675861fca5d1e2ebcdfb478abd2a31f84a598
Dein Name <deniz060198@hotmail.com>
2025-10-02 00:26:22 +0200

    better floating rules

--- dotfiles/awesome/.config/awesome/shell/windowing/rules.lua
+++ dotfiles/awesome/.config/awesome/shell/windowing/rules.lua
@@ -4,11 +4,27 @@ local beautiful = require("beautiful")
 
 local M = {}
 
+-- kleine Whitelist für GUI-Filemanager
+local FILEMANAGER_CLASSES = {
+	"Nemo",
+	"Thunar",
+	"Dolphin",
+	"org.gnome.Nautilus",
+	"Nautilus",
+	"Pcmanfm",
+	"Pcmanfm-qt",
+	"Caja",
+	"Spacefm",
+	"Krusader",
+	"Double Commander",
+}
+
 function M.apply(o)
 	local modkey = o.modkey
 	local mouse = o.mouse
 
 	awful.rules.rules = {
+		-- Default
 		{
 			rule = {},
 			properties = {
@@ -16,18 +32,52 @@ function M.apply(o)
 				border_color = beautiful.border_normal,
 				focus = awful.client.focus.filter,
 				raise = true,
-				-- keine clientkeys mehr (promote-to-master ist global)
 				buttons = mouse and mouse.client_buttons and mouse.client_buttons(modkey) or nil,
 				screen = awful.screen.preferred,
 				placement = awful.placement.no_overlap + awful.placement.no_offscreen,
 			},
 		},
+
+		-- GUI-Filemanager immer floating
+		{
+			rule_any = { class = FILEMANAGER_CLASSES },
+			properties = {
+				floating = true,
+				placement = awful.placement.centered,
+			},
+		},
+
+		-- „Werkzeug“-Fenster & Popups generell floating (deckt sehr viel ab)
+		{
+			rule_any = {
+				type = { "dialog", "utility", "toolbar", "splash" },
+				role = { "pop-up", "Preferences" },
+			},
+			properties = {
+				floating = true,
+				placement = awful.placement.centered,
+			},
+		},
+
+		-- Blueman Manager zuverlässig erwischen (class *oder* instance)
+		{
+			rule_any = {
+				class = { "Blueman-manager", "blueman-manager", "Blueman" },
+				instance = { "blueman-manager" },
+				name = { "Bluetooth", "Blueman" }, -- Falls class/instance abweicht
+			},
+			properties = {
+				floating = true,
+				placement = awful.placement.centered,
+			},
+		},
+
+		-- Sonstige Utilities (deine Liste)
 		{
 			rule_any = {
 				instance = { "DTA", "copyq", "pinentry" },
 				class = {
 					"Arandr",
-					"Blueman-manager",
 					"Gpick",
 					"Kruler",
 					"MessageWin",
@@ -36,12 +86,14 @@ function M.apply(o)
 					"Wpa_gui",
 					"veromix",
 					"xtightvncviewer",
+					-- "Blueman-manager", -- bereits oben abgedeckt
 				},
 				name = { "Event Tester" },
-				role = { "AlarmWindow", "ConfigManager", "pop-up" },
 			},
 			properties = { floating = true },
 		},
+
+		-- Titlebars für „normal“ & „dialog“
 		{
 			rule_any = { type = { "normal", "dialog" } },
 			properties = { titlebars_enabled = true },
